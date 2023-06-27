9ef702a063d99c263d1a2ad3912ad1b2ee6933f2
RasikaRavi <rasikaravi2020@gmail.com>
2023-06-27 15:22:03 +0530

    TaskList added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,15 +5,18 @@ import ProjectCreate from "./components/ProjectCreate/ProjectCreate";
 import TaskCreation from "./components/TaskCreation/TaskCreation";
 import EditTask from "./components/EditTask/EditTask";
 import EditProject from "./components/EditProject/EditProject";
+import TaskDetails from "./pages/TaskDetails";
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/taskcreation" element={<TaskCreation/>}/>
+        <Route path="/taskcreation" element={<TaskCreation />} />
         <Route path="/edittask" element={<EditTask />} />
-        <Route path="/projects" element={<ProjectCreate/>}/>
-        <Route path="/editproject"element={<EditProject/>}/>{/* change path once merge backend */}
+        <Route path="/projects" element={<ProjectCreate />} />
+        <Route path="/editproject" element={<EditProject />} />
+        {/* change path once merge backend */}
+        <Route path="/tasklist" element={<TaskDetails />} />
       </Routes>
     </div>
   );
