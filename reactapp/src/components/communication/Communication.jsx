ce38636d64d11dff03cdc3766dcc477c3d2c569c
B. Sai kishore <b.saikishore1611@gmail.com>
2023-07-25 17:46:09 +0530

    added communication page

--- reactapp/src/components/communication/Communication.jsx
+++ reactapp/src/components/communication/Communication.jsx
@@ -1,152 +1,219 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-//import './communication.css';
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import api, { BASE_URL } from "../../utils/api";
 
 const Communication = () => {
-  const [projectid, setProjectid] = useState(0);
-  const [chats, setChat] = useState([]);
-  const[sendMessage,setsendMessage]=useState({
-    
-    "name":'',
-    "description":''
-  });
-  const onInputchange = (e) => {
-    setProjectid(e.target.value);
-  };
-  const getChat = (e) => {
-    if (projectid > 0) {
-      console.log('projectId = ' + projectid);
-      loadUser();
-    } else {
-      console.log('less than 0');
-    }
-  };
+  const decodedToken = JSON.parse(localStorage.getItem("decodedToken"));
+  const [chats, setChats] = useState([]);
+  const [project, setProject] = useState({});
+  const name = decodedToken.username;
+  const formRef = useRef(null);
+  const messageEndRef = useRef(null);
   useEffect(() => {
-    console.log("projectid = "+projectid+" name = "+sendMessage.name+" description = "+sendMessage.description);
-    console.log("\n send projectid = "+sendMessage.projectid);    
-    const interval  = setInterval(() =>{
+    if (decodedToken.role === "MANAGER") {
+      const loginid = decodedToken.id;
+      api
+        .get(`${BASE_URL}/project/manager/${loginid}`)
+        .then((response) => {
+          setProject(response.data);
+          setSendMessage((prevSendMessage) => ({
+            ...prevSendMessage,
+            projectid: response.data.id,
+          }));
+        })
+        .catch((error) => {
+          console.error("Error fetching options: ", error);
+        });
+    } else {
+      const loginid = decodedToken.id;
+      try {
+        api
+          .get(`${BASE_URL}/project/user/${loginid}`)
+          .then((response) => {
+            setProject(response.data);
+            setSendMessage((prevSendMessage) => ({
+              ...prevSendMessage,
+              projectid: response.data.project_id,
+            }));
+          })
+          .catch((error) => {
+            console.error("Error fetching options: ", error);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [decodedToken.id, decodedToken.role]);
+
+  useEffect(() => {
+    messageEndRef.current?.scrollIntoView();
+  }, [chats]);
+  const [sendMessage, setSendMessage] = useState({
+    name: decodedToken.username,
+    description: "",
+    projectid:
+      decodedToken.role === "MANAGER" ? project.id : project.project_id,
+  });
+
+  const getChat = () => {
+    loadUser();
+  };
+  const loadUser = useCallback(async () => {
+    const pid = sendMessage.projectid;
+    try {
+      const response = await api.get(`${BASE_URL}/communication/${pid}`);
+      setChats(response.data);
+    } catch (error) {
+      console.error("Error loading project:", error);
+    }
+  }, [sendMessage.projectid]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
       loadUser();
     }, 5000);
+
     return () => {
       clearInterval(interval);
     };
-    
-  },[sendMessage,projectid])
-  const loadUser = async () => {
-    try {
-      const response = await axios.get(
-        'http://localhost:8080/communication?projectid=' + projectid
-      );
-      setChat(response.data);
-    } catch (error) {
-      console.error('Error loading project:', error);
-    }
+  }, [loadUser]);
+
+  const addFormdata = (e) => {
+    setSendMessage({ ...sendMessage, [e.target.name]: e.target.value });
   };
-  const addFormdata =(e)=>{
-    setsendMessage({...sendMessage,[e.target.name]: e.target.value});
-    
-  };
-   const onSubmit=async(e)=>{
-     e.preventDefault();
-     if(sendMessage.projectid>0)
-     {
-    await axios.post("http://localhost:8080/addchat",sendMessage);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const projectid = sendMessage.projectid;
+    await api.post(`${BASE_URL}/addchat/${projectid}`, sendMessage);
     loadUser();
-    console.log("data added");
-     }
-     else
-     {
-      alert("less than 0 \n"+"project id in send = "+sendMessage.projectid+"\n"+"project name in send = "+sendMessage.name+"\nproject description in send = "+sendMessage.description);
-     }
-    
-   }
+    formRef.current.reset();
+  };
+
   return (
     <div>
-      <div className="container card shadow d-flex align-items-center justify-content-center p-2 m-2 mx-auto">
-        <div className="input-group text-center mx-auto d-flex align-items-center justify-content-center">
-          <input
-            type="number"
-            placeholder="Enter project id..."
-            name="projectid"
-            className="pidbox p-2"
-            onChange={(e) => onInputchange(e)}
+      {sendMessage.projectid === undefined ? (
+        <div className="text-center mt-5">
+          <img
+            src="https://www.itarian.com/assets-new/images/project-management.png"
+            height="450px"
+            width="500px"
+            style={{ maxWidth: "100%" }}
+            alt=""
           />
-          <input
-            type="text"
-            name="name"
-            className="p-3"
-            placeholder="Enter a name"
-            style={{ width: '200px', height:"40px" }}
-            onChange={(e)=>addFormdata(e)}
-            required
-          />
-          <button
-            className="bg-success text-white p-2"
-            style={{ width: '70px' }}
-            onClick={(e) => getChat(e)}
+          <h2 className="text-primary">
+            {" "}
+            <br />
+            No projects found
+          </h2>
+          <p style={{ fontSize: "18px" }}>
+            It appears that no projects have yet been created! Once your project
+            is created, the project chats will be displayed here.{" "}
+          </p>
+        </div>
+      ) : (
+        <div>
+          <div className="container card shadow d-flex align-items-center justify-content-center p-2 m-2 mx-auto">
+            <div className="input-group text-center mx-auto d-flex align-items-center justify-content-center">
+              <input
+                type="number"
+                placeholder="Enter project id..."
+                name="projectid"
+                className="pidbox p-2"
+                value={sendMessage.projectid}
+                disabled
+              />
+              <input
+                type="text"
+                name="name"
+                className="p-3"
+                value={name}
+                placeholder="Enter a name"
+                style={{ width: "200px", height: "40px" }}
+                onChange={addFormdata}
+                required
+                disabled
+              />
+              <button
+                className="bg-success text-white p-2"
+                style={{ width: "70px" }}
+                onClick={getChat}
+              >
+                chat
+              </button>
+            </div>
+          </div>
+
+          <div
+            className="container communicationbody border border-solid border-1 border-danger"
+            style={{
+              maxHeight: "550px",
+              overflowX: "hidden",
+              overflowY: "scroll",
+            }}
           >
-            chat
-          </button>
+            {chats.map((chat) => {
+              if (chat.name === sendMessage.name) {
+                return (
+                  <h6
+                    className="ms-auto m-1 p-3 border border-1 border-black"
+                    style={{ width: "500px" }}
+                    key={chat.id}
+                  >
+                    <span className="text-info">{name} : </span>
+                    {chat.description}
+                  </h6>
+                );
+              } else {
+                return (
+                  <h6
+                    className="me-auto m-1 p-3 border border-1 border-black"
+                    style={{ width: "500px" }}
+                    key={chat.id}
+                  >
+                    <span className="text-info">{chat.name} : </span>
+                    {chat.description}
+                  </h6>
+                );
+              }
+            })}
+            <div ref={messageEndRef} />
+          </div>
+
+          <div className="container card shadow d-flex align-items-center justify-content-center p-3 m-2 mx-auto">
+            <form ref={formRef} onSubmit={onSubmit}>
+              <div className="input-group">
+                <input
+                  type="number"
+                  name="projectid"
+                  className="p-3"
+                  value={sendMessage.projectid}
+                  style={{ width: "80px" }}
+                  onChange={addFormdata}
+                  required
+                  disabled
+                />
+
+                <input
+                  type="text"
+                  name="description"
+                  placeholder="Enter a message"
+                  className="p-3"
+                  style={{ width: "900px" }}
+                  onChange={addFormdata}
+                  required
+                />
+                <button
+                  className="p-3 bg-success text-white"
+                  style={{ width: "80px" }}
+                  disabled={sendMessage.projectid === undefined}
+                >
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-      <div className="container communicationbody border border-solid border-1 border-danger">
-      
-        {chats.map((chat) => {
-          if(chat.projectid==projectid)
-          if (chat.name ===  sendMessage.name) {
-            return (
-              <h6
-                className="ms-auto m-1 p-3 border border-1 border-black"
-                style={{ width: '500px' }}
-              >
-                <span className="text-info">{chat.name} :  </span>
-                {chat.description} 
-                
-              </h6>
-            );
-          } else {
-            return (
-              <h6
-                className="me-auto m-1 p-3 border border-1 border-black"
-                style={{ width: '500px' }}
-              >
-                <span className="text-info" >{chat.name} :  </span>{chat.description}
-              </h6>
-            );
-          }
-        })}
-      </div>
-      <div className="container card shadow d-flex align-items-center justify-content-center p-3 m-2 mx-auto">
-        <form onSubmit={(e)=>onSubmit(e)}>
-        <div className="input-group">
-          <input
-            type="number"
-            name="projectid"
-            className="p-3"
-            value={sendMessage.projectid=projectid}
-            style={{ width: '80px' }}
-            onChange={(e)=>addFormdata(e)}
-            required
-           disabled
-          />
-          
-          
-          
-          <input
-            type="text"
-            name="description"
-            placeholder="Enter a message"
-            className="p-3"
-            style={{ width: '900px' }}
-            onChange={(e)=>addFormdata(e)}
-            required
-          />
-          <button className="p-3 bg-success text-white" style={{ width: '80px' }}>
-            Send
-          </button>
-        </div>
-        </form>
-      </div>
+      )}
     </div>
   );
 };
